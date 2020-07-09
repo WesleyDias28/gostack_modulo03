fd001fb48f9d6f06e1b3ff1de2525f671a0d5040
Wesley <wesley.dias28@hotmail.com>
2020-07-09 04:01:15 -0300

    new commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e073de8251ca2f',
+    pass: '8bdf938da35074',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
